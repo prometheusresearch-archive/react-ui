0e02a4fe79ba060e8f073e1bf76b961ba02d90c5
Andrey Popp <8mayday@gmail.com>
2016-05-24 14:56:38 +0300

    feat: add <ButtonBase />

---
+++ src/ButtonBase.js
@@ -0,0 +1,112 @@
+/**
+ * @copyright 2015, Prometheus Research, LLC
+ */
+
+import * as React from 'react';
+import * as Stylesheet from 'react-stylesheet';
+import {style} from 'react-dom-stylesheet';
+import * as CSS from 'react-dom-stylesheet/css';
+import {isString} from 'lodash';
+
+
+let stylesheet = Stylesheet.create({
+  Root: style('button', {
+    cursor: CSS.cursor.pointer,
+    textAlign: CSS.textAlign.left,
+    userSelect: CSS.none,
+    WebkitUserSelect: CSS.none,
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+  }),
+  Caption: style('div', {
+    display: CSS.display.inlineBlock,
+  }),
+  Icon: 'span',
+  IconWrapper: style('div', {
+    display: CSS.display.inlineBlock,
+  }),
+});
+
+/**
+ * Button component.
+ *
+ * Button is clickable element with optional icon and/or caption.
+ */
+export default class ButtonBase extends React.Component {
+
+  static propTypes = {
+    /**
+     * If button should be rendered as being pressed.
+     */
+    active: React.PropTypes.bool,
+
+    /**
+     * Button size.
+     */
+    size: React.PropTypes.oneOf(['small', 'normal', 'large']),
+
+    /**
+     * Button's icon.
+     */
+    icon: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.node
+    ]),
+
+    /**
+     * Button's caption.
+     */
+    children: React.PropTypes.node,
+  };
+
+  static defaultProps = {
+    size: 'normal',
+    attach: {},
+    stylesheet,
+  };
+
+  render() {
+    let {
+      children, icon,
+      disabled, active, size,
+      attach,
+      variant,
+      stylesheet: {Root, Caption, Icon, IconWrapper},
+      ...props
+    } = this.props;
+    variant = {
+      active, disabled,
+      attachLeft: attach.left,
+      attachRight: attach.right,
+      attachTop: attach.top,
+      attachBottom: attach.bottom,
+      small: size === 'small',
+      normal: size === 'normal',
+      large: size === 'large',
+      ...variant,
+    };
+    if (isString(icon)) {
+      icon = <Icon name={icon} />;
+    }
+    let caption = null;
+    if (children) {
+      caption = <Caption>{children}</Caption>;
+    }
+    return (
+      <Root
+        {...props}
+        disabled={disabled}
+        variant={variant}
+        aria-pressed={active}
+        role="button">
+        {icon ?
+          <IconWrapper variant={{hasCaption: !!children}}>
+            {icon}
+          </IconWrapper> :
+          null}
+        {caption}
+      </Root>
+    );
+  }
+}
+
