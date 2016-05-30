93ac238c6117a1bdbc8a539503c886e6f7434275
Andrey Popp <8mayday@gmail.com>
2016-05-30 12:43:04 +0300

    doc: button demo

--- site/Site.js
+++ site/Site.js
@@ -1,54 +1,60 @@
 import 'normalize.css/normalize.css';
 import React from 'react';
 import * as ReactUI from '../src/index';
-
-function Block({children}) {
-  return (
-    <div style={{marginBottom: 10}}>
-      {children}
-    </div>
-  );
-}
+import {Block} from '../src/index';
 
 function Item({children}) {
   return (
-    <div style={{display: 'inline-block', marginRight: 10}}>
+    <Block inline marginRight="x-small">
       {children}
-    </div>
+    </Block>
   );
+}
+
+function Items({children}) {
+  return <Block marginBottom="small">{children}</Block>;
 }
 
 export default function Site() {
   return (
-    <div>
-      <Block>
+    <Block padding="small">
+      <Items>
+
         <Item>
           <ReactUI.Button size="small">Button</ReactUI.Button>
         </Item>
         <Item>
-          <ReactUI.Button size="small" variant={{hover: true}}>Button</ReactUI.Button>
+          <ReactUI.Button size="small" variant={{hover: true}}>
+            Button
+          </ReactUI.Button>
         </Item>
         <Item>
-          <ReactUI.Button size="small" variant={{focus: true}}>Button</ReactUI.Button>
+          <ReactUI.Button size="small" variant={{focus: true}}>
+            Button
+          </ReactUI.Button>
         </Item>
         <Item>
-          <ReactUI.Button size="small" variant={{active: true}}>Button</ReactUI.Button>
+          <ReactUI.Button size="small" variant={{active: true}}>
+            Button
+          </ReactUI.Button>
         </Item>
         <Item>
-          <ReactUI.Button size="small" disabled>Button</ReactUI.Button>
+          <ReactUI.Button size="small" disabled>
+            Button
+          </ReactUI.Button>
         </Item>
 
-      </Block>
+      </Items>
 
-      <Block>
+      <Items>
         <Item>
           <ReactUI.Button groupHorizontally size="small">Left</ReactUI.Button>
           <ReactUI.Button groupHorizontally size="small">Center</ReactUI.Button>
           <ReactUI.Button groupHorizontally size="small">Right</ReactUI.Button>
         </Item>
-      </Block>
+      </Items>
 
-      <Block>
+      <Items>
         <Item>
           <ReactUI.Button>Button</ReactUI.Button>
         </Item>
@@ -64,8 +70,8 @@ export default function Site() {
         <Item>
           <ReactUI.Button disabled>Button</ReactUI.Button>
         </Item>
-      </Block>
-      <Block>
+      </Items>
+      <Items>
         <Item>
           <ReactUI.Button size="large">Button</ReactUI.Button>
         </Item>
@@ -82,9 +88,9 @@ export default function Site() {
           <ReactUI.Button size="large" disabled>Button</ReactUI.Button>
         </Item>
 
-      </Block>
+      </Items>
 
-      <Block>
+      <Items>
         <Item>
           <ReactUI.FlatButton size="small">Button</ReactUI.FlatButton>
         </Item>
@@ -94,8 +100,8 @@ export default function Site() {
         <Item>
           <ReactUI.FlatButton size="large">Button</ReactUI.FlatButton>
         </Item>
-      </Block>
-      <Block>
+      </Items>
+      <Items>
         <Item>
           <ReactUI.QuietButton size="small">Button</ReactUI.QuietButton>
         </Item>
@@ -105,8 +111,8 @@ export default function Site() {
         <Item>
           <ReactUI.QuietButton size="large">Button</ReactUI.QuietButton>
         </Item>
-      </Block>
-      <Block>
+      </Items>
+      <Items>
         <Item>
           <ReactUI.DangerButton size="small">Button</ReactUI.DangerButton>
         </Item>
@@ -116,8 +122,8 @@ export default function Site() {
         <Item>
           <ReactUI.DangerButton size="large">Button</ReactUI.DangerButton>
         </Item>
-      </Block>
-      <Block>
+      </Items>
+      <Items>
         <Item>
           <ReactUI.FlatDangerButton size="small">Button</ReactUI.FlatDangerButton>
         </Item>
@@ -127,8 +133,8 @@ export default function Site() {
         <Item>
           <ReactUI.FlatDangerButton size="large">Button</ReactUI.FlatDangerButton>
         </Item>
-      </Block>
-      <Block>
+      </Items>
+      <Items>
         <Item>
           <ReactUI.SuccessButton size="small">Button</ReactUI.SuccessButton>
         </Item>
@@ -138,7 +144,7 @@ export default function Site() {
         <Item>
           <ReactUI.SuccessButton size="large">Button</ReactUI.SuccessButton>
         </Item>
-      </Block>
-    </div>
+      </Items>
+    </Block>
   );
 }
