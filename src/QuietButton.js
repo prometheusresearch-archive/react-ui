fe8b323b673d0d0994e6ffedfbd76d0afd51511c
Andrey Popp <8mayday@gmail.com>
2017-02-24 12:55:08 +0300

    feat: new styles for <QuietButton />

--- src/QuietButton.js
+++ src/QuietButton.js
@@ -17,19 +17,19 @@ export default class QuietButton extends ButtonBase {
     text: css.rgb(136),
     textHover: css.rgb(68),
     textFocus: css.rgb(68),
-    textActive: css.rgb(255),
+    textActive: css.rgb(31, 133, 245),
     textDisabled: '#dadada',
 
     background: css.rgb(255),
-    backgroundHover: css.rgb(241),
+    backgroundHover: css.rgb(255),
     backgroundFocus: css.rgb(255),
-    backgroundActive: css.rgb(150),
+    backgroundActive: css.rgb(255),
     backgroundDisabled: css.rgb(255),
 
     border: css.color.transparent,
     borderHover: css.color.transparent,
     borderFocus: css.rgb(241),
-    borderActive: css.rgb(150),
+    borderActive: css.color.transparent,
 
     shadowFocus: css.none,
     shadowActive: css.none,
