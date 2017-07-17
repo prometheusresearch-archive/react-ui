67489ea63ff03b92deba7189ec06b1f392db3b1d
Andrey Popp <8mayday@gmail.com>
2017-07-17 16:16:13 +0300

    feat: allow passing nulls as tabs to <TabContainer />

--- src/TabContainer.js
+++ src/TabContainer.js
@@ -13,8 +13,8 @@ type TabSpec = {
 };
 
 type TabContainerProps = {
-  tabList: Array<TabSpec>,
-  tabListAlt: Array<TabSpec>,
+  tabList: Array<?TabSpec>,
+  tabListAlt: Array<?TabSpec>,
   activeTab: string,
   onActiveTab: (string) => void,
 };
@@ -28,37 +28,35 @@ export default class TabContainer extends React.Component {
   };
 
   render() {
-    const {tabList, tabListAlt, activeTab, onActiveTab} = this.props;
+    const {tabList, tabListAlt, activeTab} = this.props;
     const tabListAll = tabList.concat(tabListAlt);
-    const found = tabListAll.find(tab => tab.id === activeTab);
-    invariant(found, 'Cannot find active tab');
+    const found = tabListAll.find(tab => tab && tab.id === activeTab);
+    invariant(found, 'Cannot find an active tab');
     return (
       <VBox flexGrow={1}>
         <HBox justifyContent="space-between" borderBottom="1px solid #BBBBBB">
           <HBox>
-            {tabList.map(tab => (
-              <TabButton
-                label={tab.label || tab.id}
-                active={activeTab === tab.id}
-                key={tab.id}
-                onClick={() => onActiveTab(tab.id)}
-              />
-            ))}
+            {tabList.map(tab => tab ? this.renderTab(tab) : null)}
           </HBox>
           {tabListAlt.length > 0 &&
             <HBox>
-              {tabListAlt.map(tab => (
-                <TabButton
-                  label={tab.label || tab.id}
-                  active={activeTab === tab.id}
-                  key={tab.id}
-                  onClick={() => onActiveTab(tab.id)}
-                />
-              ))}
+              {tabListAlt.map(tab => tab ? this.renderTab(tab) : null)}
             </HBox>}
         </HBox>
         {found.children}
       </VBox>
+    );
+  }
+
+  renderTab(tab: TabSpec) {
+    const {activeTab, onActiveTab} = this.props;
+    return (
+      <TabButton
+        label={tab.label || tab.id}
+        active={activeTab === tab.id}
+        key={tab.id}
+        onClick={() => onActiveTab(tab.id)}
+      />
     );
   }
 }
